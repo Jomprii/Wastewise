5eae35631ee7dd106b89699bbd95d7c5072bffb6
Jomprii <jyamson30@gmail.com>
2025-08-06 17:28:15 +0800

    Removed Login and Signup

--- App.js
+++ App.js
@@ -2,15 +2,12 @@ import React, { useEffect, useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Loadscrn from "./assets/screens/Loadingscreen";
-import Login from "./assets/screens/Login";
-import signup from "./assets/screens/Signup";
 import home from "./assets/screens/Dashboard";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   const [isLoading, setIsLoading] = useState(true);
-  const [userToken, setUserToken] = useState(null);
 
   useEffect(() => {
     setTimeout(() => {
@@ -25,9 +22,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Dashboard" component={home} />
-        <Stack.Screen name="Signup" component={signup} />
       </Stack.Navigator>
     </NavigationContainer>
   );
